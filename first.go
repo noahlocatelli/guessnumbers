530d01d3e29994e6f2eb9bd2668434b3019f1cad
locatelli <noah.locatelli@ynov.com>
2020-09-14 09:38:16 +0200

    plusoumoinsfinit

--- first.go
+++ first.go
@@ -7,13 +7,19 @@ import (
 )
 
 func main()  {
-		fmt.Print("moins")
-		rand.Seed(time.Now().UnixNano())
-		var number = rand.Intn(100)
+	rand.Seed(time.Now().UnixNano())
+
+	var number = rand.Intn(100)
+	for  {
+
 		var try int
-		fmt.Print("choisir un nombre")
+		fmt.Println("choisir un nombre")
 		fmt.Scan(&try)
 
-		for try<number{
+		if try<number {
+			fmt.Println("plus")
+
+		} else if try>number {fmt.Println("moins")} else if try==number{fmt.Print("égale")}
+break
 	}
 }
